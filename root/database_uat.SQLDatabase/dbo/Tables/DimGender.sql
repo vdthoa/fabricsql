e04af7f308252e9974808ab00f91783305e96605
vdthoa <59270538+vdthoa@users.noreply.github.com>
2024-11-21 22:21:17 +0700

    add phone number to dim gender

--- root/database_uat.SQLDatabase/dbo/Tables/DimGender.sql
+++ root/database_uat.SQLDatabase/dbo/Tables/DimGender.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[DimGender] (
     [Id]         INT           NOT NULL,
     [GenderName] NVARCHAR (50) NOT NULL,
     [FullName]   VARCHAR (200) NULL,
+    [Address]    VARCHAR (200) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
